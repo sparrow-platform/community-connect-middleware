3f39719dbde7e1a5c823bb3dfb82dc794ab2489c
Jay Lohokare <jaylohokare@gmail.com>
2019-05-30 00:44:14 -0400

    Converted all functions from Mongo to CouchDB

--- db.py
+++ db.py
@@ -37,124 +37,49 @@ def getReceiver(userID):
 
 
 def updateUserType(userID, type):
-    pass
+    if userID in usersDB:
+        user = usersDB[userID]
+        user['type'] = type
+        user.save()
+        return True
+    return False
 
-def setupConnection(user1, user2):
-    pass
+
+def setupConnection(user1, user2):    
+    if user1 not in usersDB or user2 not in usersDB:
+        return False
+    userDoc1 = usersDB[user1]
+    userDoc1['receiver'] = user2
+    userDoc1.save()
+    userDoc2 = usersDB[user2]
+    userDoc2['receiver'] = user1
+    userDoc2.save()
+    return True
+
 
 def breakConnection(user1, user2):
-    pass
+    if user1 not in usersDB or user2 not in usersDB:
+        return False
+    userDoc1 = usersDB[user1]
+    userDoc1['receiver'] = IBM_RECEIVER
+    userDoc1.save()
+    userDoc2 = usersDB[user2]
+    userDoc2['receiver'] = IBM_RECEIVER
+    userDoc2.save()
+    return True
+
 
 def findDoctor():
-    pass
+    #TODO Check if self is not returned
+    for user in usersDB:
+        if user['type'] == TYPE_DOCTOR and user['receiver'] == IBM_RECEIVER:
+            return user['_id']
+    return None
+
 
 def findCommunity():
-    pass
+    for user in usersDB:
+        if user['type'] == TYPE_COMMUNITY and user['receiver'] == IBM_RECEIVER:
+            return user['_id']
+    return None
 
-    
-
-# myclient = pymongo.MongoClient(config["mongo"]["client"])
-# mydb = myclient["sparrow"]
-# sessionCol = mydb["sessions"]
-# userCol = mydb["users"]
-
-# def getSessionID(userID):
-#     user = sessionCol.find_one({"userID":userID})
-#     print(user)
-#     if user:
-#         print(user)
-#         return user['sessionID']
-#     else:
-#         session = chatbot.new_session()
-#         my_dic = {'userID':userID, 'sessionID':session, 'created_at': datetime.utcnow()}
-#         sessionCol.insert_one(my_dic)
-#         return session
-
-# def getReceiver(userID):
-#     user = userCol.find_one({"userID":userID})
-#     if user:
-#         return user["receiver"]
-#     else:
-#         #new user with default to ibm_assistant
-#         my_dic = {'userID': userID, 'receiver':IBM_RECEIVER, 'type':'user'}
-#         userCol.insert_one(my_dic)
-#         return 'ibm_assistant'
-
-# def updateUserType(userID, type):
-#     userCol.update_one({
-#         'userID':userID
-#     }, {
-#         '$set':{
-#             'type':type
-#         }
-#     }, upsert=False)
-
-# def setupConnection(user1, user2):
-#     userCol.update_one({
-#         'userID':user1
-#     }, {
-#         '$set':{
-#             'receiver':user2
-#         }
-#     }, upsert=False)
-#     userCol.update_one({
-#         'userID':user2
-#     }, {
-#         '$set':{
-#             'receiver':user1
-#         }
-#     }, upsert=False)
-
-# def breakConnection(user1, user2):
-#     userCol.update_one({
-#         'userID':user1
-#     }, {
-#         '$set':{
-#             'receiver':IBM_RECEIVER
-#         }
-#     }, upsert=False)
-#     userCol.update_one({
-#         'userID':user2
-#     }, {
-#         '$set':{
-#             'receiver':IBM_RECEIVER
-#         }
-#     }, upsert=False)
-
-# def findDoctor():
-#     user = userCol.find_one({'type':TYPE_DOCTOR, 'receiver': IBM_RECEIVER})
-#     if user:
-#         return user["userID"]
-#     else:
-#         return None
-
-# def findCommunity():
-#     user = userCol.find_one({'type':TYPE_COMMUNITY, 'receiver': IBM_RECEIVER})
-#     if user:
-#         return user["userID"]
-#     else:
-#         return None
-
-
-
-
-def getSessionID(userID):
-    pass
-
-def getReceiver(userID):
-    pass
-
-def updateUserType(userID, type):
-    pass
-
-def setupConnection(user1, user2):
-    pass
-
-def breakConnection(user1, user2):
-    pass
-
-def findDoctor():
-    pass
-
-def findCommunity():
-    pass
