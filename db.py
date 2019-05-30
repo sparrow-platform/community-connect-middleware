21d9faa70e1d6f14bd081d6791259cf18e77a982
Jay Lohokare <jaylohokare@gmail.com>
2019-05-29 23:42:44 -0400

    Added get functions

--- db.py
+++ db.py
@@ -1,25 +1,40 @@
 import json
 import chatbot
 from datetime import datetime
+from cloudant.client import Cloudant
 
 TYPE_USER = 'user'
 TYPE_DOCTOR = 'doctor'
 TYPE_COMMUNITY = 'community'
 IBM_RECEIVER = 'ibm_assistant'
 
-# #https://python-cloudant.readthedocs.io/en/latest/getting_started.html
-
-
 with open('config.json') as config_file:
     config = json.load(config_file)
-
+    
+client = Cloudant.iam(config["couch"]["username"], config["couch"]["api"], connect=True)
+sessionsDB = client['sessions']
+usersDB = client['users']
 
 
 def getSessionID(userID):
-    pass
+    if userID in sessionsDB:
+        return sessionsDB[userID]['sessionID']
+    else:
+        session = chatbot.new_session()
+        my_dic = {'_id':userID, 'sessionID':session, 'created_at': str(datetime.utcnow())}
+        sessionsDB.create_document(my_dic)
+        return session
+   
+
 
 def getReceiver(userID):
-    pass
+    if userID in usersDB:
+        return usersDB[userID]["receiver"]
+    else:
+        my_dic = {'_id': userID, 'receiver':IBM_RECEIVER, 'type':'user'}
+        usersDB.create_document(my_dic)
+        return 'ibm_assistant'
+
 
 def updateUserType(userID, type):
     pass
