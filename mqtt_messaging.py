bb875375fccb4f6b69d9f9934d61af7d1bf9ea4c
mihir97 <mistrymu15.comp@coep.ac.in>
2019-06-19 18:24:59 +0530

    MQTT fix

--- mqtt_messaging.py
+++ mqtt_messaging.py
@@ -7,9 +7,11 @@ with open('config.json') as config_file:
 broker=config["mqtt"]["broker"]
 port=config["mqtt"]["port"]
 
-client= paho.Client("sparrow-middleware")
-client.connect(broker,port)
 
 def send_message(userID, message):
+    client= paho.Client("sparrow-middleware")
+    client.connect(broker,port)
     topic = "sparrow_response/"+userID
-    client.publish(topic,message)
+    ret  = client.publish(topic,message)
+    print(ret, topic, message)
+    client.disconnect()
